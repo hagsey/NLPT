4bd005ce85390cbea54269fa109ba328cc441763
Adge <adgekrebs@gmail.com>
2016-03-01 21:14:54 -0500

    Worked on footers schedule

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -54,23 +54,8 @@
 		<div class="third-section">
 			<div class=container-fluid>
 				<div class="row">
-					<h1>What's New?</h1>
-					<div class="col-md-4 col-md-offset-1 recent-blog">
-						<h2>Newest Blog entry</h2>
-						<div class="blog-post">
-							<h3>An Easy Way to Improve Your Grip (And Overall Strength)</h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-							proident, sunt in culpa qui officia deserunt ollit anim id est laborum.
-							</p>
-						</div>
-					</div>
-					<div class="col-md-4 col-md-offset-1 video">
-						<h2>Recent Video</h2>
-						<iframe allowfullscreen="" src="https://www.youtube.com/embed/QO7obYANRCM?rel=0" width="420" frameborder="0" height="315"></iframe>
+					<div class="col-md-6 col-md-offset-3 title-box">
+						<h1>What's New?</h1>
 					</div>
 				</div>
 			</div>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,60 +1,154 @@
 <div class="footer container-fluid">
-
 	<div class="row">
-		<div class="col-md-1 schedule-day">
-			<div class="panel panel-primary">
-				<div class="panel-heading">Monday</div>
-				<div class="panel-body">
-					stuff
+		<div class="col-md-4 schedule">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 class="schedule-title">Hours</h3>
+				</div>
+				<div class="col-md-8">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="btn-group schedule-btn-group" role="group" aria-label="...">
+								<button class="btn btn-default btn-xs clicked">Cambridge</button>
+								<button class="btn btn-default btn-xs">Stratford</button>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="btn-group schedule-btn-group" role="group" aria-label="...">
+								<button class="btn btn-default btn-xs">Athlete</button>
+								<button class="btn btn-default btn-xs clicked">Fit</button>
+								<button class="btn btn-default btn-xs">Team</button>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div class="col-md-1 schedule-day">
-			<div class="panel panel-primary">
-				<div class="panel-heading">Tuesday</div>
-				<div class="panel-body">
-					stuff
+			<div class="row">
+				<div class="col-md-12">
+					<table class= "table">
+						<th>
+							<table class= "table table-condensed">
+								<thead>
+									<tr>
+										<th class="day-header">Mon</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<th>6-9 am</th>
+									</tr>
+									<tr>
+										<th>12-1 pm</th>
+									</tr>
+									<tr>
+										<th>5-6:30 pm</th>
+									</tr>
+								</tbody>
+							</table>
+						</th>
+						<th>
+							<table class= "table table-condensed">
+								<thead>
+									<tr>
+										<th class="day-header">Tue</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<th>6-9 am</th>
+									</tr>
+									<tr>
+										<th>2-3 pm</th>
+									</tr>
+									<tr>
+										<th>5-6:30 pm</th>
+									</tr>
+								</tbody>
+							</table>
+						</th>
+						<th>
+							<table class= "table table-condensed">
+								<thead>
+									<tr>
+										<th class="day-header">Wed</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<th>6-9 am</th>
+									</tr>
+									<tr>
+										<th>12-1 pm</th>
+									</tr>
+									<tr>
+										<th>5-6:30 pm</th>
+									</tr>
+								</tbody>
+							</table>
+						</th>
+						<th>
+							<table class= "table table-condensed">
+								<thead>
+									<tr>
+										<th class="day-header">Thur</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<th>6-9 am</th>
+									</tr>
+									<tr>
+										<th>2-3 pm</th>
+									</tr>
+									<tr>
+										<th>5-6:30 pm</th>
+									</tr>
+								</tbody>
+							</table>
+						</th>
+						<th>
+							<table class= "table table-condensed">
+								<thead>
+									<tr>
+										<th class="day-header">Fri</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<th>6-9 am</th>
+									</tr>
+									<tr>
+										<th>12-1 pm</th>
+									</tr>
+									<tr>
+										<th>5-6:30 pm</th>
+									</tr>
+								</tbody>
+							</table>
+						</th>
+						<th>
+							<table class= "table table-condensed">
+								<thead>
+									<tr>
+										<th class="day-header">Sat</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<th>8-10 am</th>
+									</tr>
+									<tr>
+										<th>--</th>
+									</tr>
+									<tr>
+										<th>--</th>
+									</tr>
+								</tbody>
+							</table>
+						</th>
+					</table>
 				</div>
-			</div>
-		</div>
-		<div class="col-md-1 schedule-day">
-			<div class="panel panel-primary">
-				<div class="panel-heading">Wednesday</div>
-				<div class="panel-body">
-					stuff
-				</div>
-			</div>
-		</div>
-		<div class="col-md-1 schedule-day">
-			<div class="panel panel-primary">
-				<div class="panel-heading">Thursday</div>
-				<div class="panel-body">
-					stuff
-				</div>
-			</div>
-		</div>
-		<div class="col-md-1 schedule-day">
-			<div class="panel panel-primary">
-				<div class="panel-heading">Friday</div>
-				<div class="panel-body">
-					stuff
-				</div>
-			</div>
-		</div>
-		<div class="col-md-1 schedule-day">
-			<div class="panel panel-primary">
-				<div class="panel-heading">Saturday</div>
-				<div class="panel-body">
-					stuff
-				</div>
-			</div>
-		</div>
-		<div class="col-md-1 schedule-day">
-			<div class="panel panel-primary">
-				<div class="panel-heading">Sunday</div>
-				<div class="panel-body">
-					stuff
-				</div>
+				
 			</div>
 		</div>
 	</div>
